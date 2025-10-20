90904c9017c86cc39cd3d2774a7248ec0b5ca4b0
abl00mfield <amanda.bloomfield@gmail.com>
2025-10-20 16:00:38 -0600

    added validation middleware

--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,34 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
-
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTest } from '../env.ts'
 
 const app = express()
 
+//mount middleware
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev', {
+    skip: () => isTest(),
+}))
+
 app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'OK',
-        timestamp: new Date().toISOString(),
-        service: 'Habit Tracker API'
-    })        
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    service: 'Habit Tracker API',
+  })
 })
 
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/habits', habitRoutes)
 
- 
 export { app }
 
 export default app
